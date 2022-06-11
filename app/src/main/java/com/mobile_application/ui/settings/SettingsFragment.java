ef8f624550e17895e4c33d12aac391ea34d5e2ff
Kev1n <ChenZihang@njust.edu.cn>
2022-06-11 17:41:52 +0800

    replace fragment name and icon
    add auto login and remember password

---
+++ app/src/main/java/com/mobile_application/ui/settings/SettingsFragment.java
@@ -0,0 +1,37 @@
+package com.mobile_application.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mobile_application.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding viewBinding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        viewBinding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = viewBinding.getRoot();
+
+        final TextView textView = viewBinding.textSettings;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        viewBinding = null;
+    }
+}
